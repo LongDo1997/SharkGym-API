11a2efe583ce3f7730ab59f85a7719a2e74ed1cc
LongDo1997 <92436024+LongDo1997@users.noreply.github.com>
2023-02-19 10:42:19 +0700

    first commit

---
+++ src/main/java/com/spring/sharkGym/SharkGymApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sharkGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkGymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharkGymApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sharkGym/SharkGymApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sharkGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkGymApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
